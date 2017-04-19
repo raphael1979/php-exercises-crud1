c58f43a8fcfa4c68afcf6582116d72fc4da59b2d
raphael <moudens.raphael@orange.fr>
2017-04-19 16:57:28 +0200

    exo 4 en cours

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	<title>colyseum</title>
 </head>
 <body>	
-	<h2>exo1</h2>
 	<?php 
 	try{
 		$dbh = new PDO('mysql:host=localhost;dbname=colyseum','root', 'root');
@@ -14,6 +13,9 @@
 		print"Error !:".$e->getMessage()."<br/>";
 		die();
 	}
+	?>
+	<h2>exo1</h2>
+	<?php
 	foreach ($dbh->query('SELECT * from clients') as $row){
 		echo $row['lastName']." ".$row['firstName']."<br/> " ;
 	}
@@ -21,10 +23,27 @@
 	<h2>exo2</h2>
 	<?php 
 
-
-
-
-
+	foreach ($dbh->query('SELECT * from showTypes') as $row){
+		echo utf8_encode($row['type'])."<br/> " ;
+	}
+	?>	
+	<h2>exo3</h2>
+	<?php 
+	foreach ($dbh->query('SELECT * from clients LIMIT 20') as $row){
+		echo $row['lastName']." ".$row['firstName']."<br/> " ;
+	}
+	?>
+	<h2>exo4</h2>
+	<?php 
+	foreach ($dbh->query('SELECT * from clients') as $row){
+		echo $row['']." ".$row['firstName']."<br/> " ;
+	}
+	?>
+	<h2>exo5</h2>
+	<?php
+	foreach ($dbh->query('SELECT * from clients') as $row){
+		echo $row['lastName']." ".$row['firstName']."<br/> " ;
+	}
 	?>
 </body>
 </html>
