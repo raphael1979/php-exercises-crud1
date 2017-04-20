bfae109a3a3dcd3064044d482b1b0ad980f9708f
raphael <moudens.raphael@orange.fr>
2017-04-20 13:50:31 +0200

    exo 4 en cours

--- index.php
+++ index.php
@@ -35,9 +35,21 @@
 	?>
 	<h2>exo4</h2>
 	<?php 
-	foreach ($dbh->query('SELECT * from clients') as $row){
-		echo $row['']." ".$row['firstName']."<br/> " ;
+	echo '<h2>Exo 4 : N\'afficher que les clients possédant une carte de fidélité.</h2>';
+	$requestCarteFidelite = 'SELECT * FROM colyseum.clients JOIN colyseum.cards ON colyseum.clients.cardNumber = colyseum.cards.cardNumber WHERE cardTypesId = 1';
+	$resultCarteFidelite = $connexion->query($requestCarteFidelite);
+	foreach ($resultCarteFidelite  as $row) {
+		print $row['id'] . " ";
+		print  $row['firstName'] . " ";
+		print $row['lastName'] . " ";
+		print $row['birthDate'] . " ";
+		print $row['card'] . " ";
+		print $row['cardNumber'] . " ";
+
+		echo'<br />';
 	}
+	
+
 	?>
 	<h2>exo5</h2>
 	<?php
