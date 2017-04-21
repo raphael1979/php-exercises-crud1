cef57a41952f4eb2cd1acd8abf2fdbf9c3381b4d
raphael <moudens.raphael@orange.fr>
2017-04-21 12:24:09 +0200

    exo 4 et 5 ok

--- index.php
+++ index.php
@@ -34,29 +34,19 @@
 	}
 	?>
 	<h2>exo4</h2>
+
 	<?php 
-	echo '<h2>Exo 4 : N\'afficher que les clients possédant une carte de fidélité.</h2>';
-	$requestCarteFidelite = 'SELECT * FROM colyseum.clients JOIN colyseum.cards ON colyseum.clients.cardNumber = colyseum.cards.cardNumber WHERE cardTypesId = 1';
-	$resultCarteFidelite = $connexion->query($requestCarteFidelite);
-	foreach ($resultCarteFidelite  as $row) {
-		print $row['id'] . " ";
-		print  $row['firstName'] . " ";
-		print $row['lastName'] . " ";
-		print $row['birthDate'] . " ";
-		print $row['card'] . " ";
-		print $row['cardNumber'] . " ";
-
-		echo'<br />';
-	}
-	
-
-	?>
-	<h2>exo5</h2>
-	<?php
-	foreach ($dbh->query('SELECT * from clients') as $row){
-		echo $row['lastName']." ".$row['firstName']."<br/> " ;
-	}
-	?>
+	foreach ($dbh->query('SELECT * FROM clients AS C, cards AS A WHERE C.cardNumber = A.cardNumber
+		AND A.cardTypesId = 1')as $row) {
+		echo $row['lastName']." ".$row['firstName']."<br/> ";
+}
+?>
+<h2>exo5</h2>
+<?php
+foreach ($dbh->query('SELECT * from clients WHERE lastName LIKE "M%" ORDER BY lastName ASC ') as $row){
+	echo "Nom: ".$row['lastName']." "."Prénom:".$row['firstName']."<br/> " ;
+}
+?>
 </body>
 </html>
 
